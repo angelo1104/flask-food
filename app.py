045eea2e73115e929734b2060b329fd0f9f7b832
Angelo <madhav1004@icloud.com>
2021-06-30 19:33:28 +0530

    added file upload

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
-from flask import Flask
+from flask import Flask, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+ALLOWED_EXTENSIONS = [
+    "jpg",
+    "jpeg",
+    "png"
+]
 
-@app.route('/')
-def hello_world():
+
+def process_file(file, filename):
+    print(file, filename)
+    return {
+        "message": "success",
+        "filename": filename
+    }
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=["GET", "POST"])
+def process_image():
+    if request.method == "POST":
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return {
+                       "message": "Please upload file"
+                   }, 400
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return {
+                       "message": "Please upload file"
+                   }, 400
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            return process_file(file, filename)
+
+        return {
+                   "message": "Error"
+               }, 400
     return 'Hello World!'
 
 
