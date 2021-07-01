dda63e1803950b1c571b2587baa89d5171c1638f
Angelo <madhav1004@icloud.com>
2021-07-01 11:03:15 +0530

    completed

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+import tensorflow.keras
 from flask import Flask, request
 from werkzeug.utils import secure_filename
+from PIL import Image, ImageOps
+import numpy as np
 
+model = tensorflow.keras.models.load_model('keras_model.h5')
 app = Flask(__name__)
+
+classes = ["apples", "apricots", "avocados", "bananas", "blackberries", "blueberries", "cantaloupes", "cherries",
+           "coconuts", "figs", "grapefruits", "grapes", "guava", "kiwifruit", "lemons", "limes", "mangos", "olives",
+           "oranges", "passionfruit", "peaches", "pears", "pineapples", "plums", "pomegranates", "raspberries",
+           "strawberries", "tomatoes", "watermelons"]
 
 ALLOWED_EXTENSIONS = [
     "jpg",
@@ -11,10 +20,31 @@ ALLOWED_EXTENSIONS = [
 
 
 def process_file(file, filename):
-    print(file, filename)
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+    # Replace this with the path to your image
+    image = Image.open(file)
+
+    # resize the image to a 224x224 with the same strategy as in TM2:
+    # resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    # turn the image into a numpy array
+    image_array = np.asarray(image)
+
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # run the inference
+    prediction = model.predict(data)
+
     return {
         "message": "success",
-        "filename": filename
+        "fruit": str(classes[prediction.argmax()])
     }
 
 
